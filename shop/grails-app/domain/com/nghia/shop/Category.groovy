c8fd7ee6a6491f0254aa20bee4a2bd8f5cafd15a
tv98.net <tv98.net@gmail.com>
2012-10-26 10:34:49 +0000

    change "def" to "String"

--- shop/grails-app/domain/com/nghia/shop/Category.groovy
+++ shop/grails-app/domain/com/nghia/shop/Category.groovy
@@ -5,9 +5,9 @@ class Category {
 	/**
 	 * If this category is sub-cat, it will be set = categoryId
 	 */
-	def subCategory
-	def name
-	static hasMany = [products : Product]
+	int subCategory
+	String name
+	//static hasMany = [products : Product]
     static constraints = {
     }
 }
