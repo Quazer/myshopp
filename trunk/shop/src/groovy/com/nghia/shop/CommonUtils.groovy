c00f6a06a7d24b4954cef53ea0ed484911f61bb3
tv98.net@gmail.com <tv98.net@gmail.com@c073b083-006d-3139-c555-1cae7268bc85>
2012-11-06 11:27:58 +0000

    add method validateParamsForPaging

--- trunk/shop/src/groovy/com/nghia/shop/CommonUtils.groovy
+++ trunk/shop/src/groovy/com/nghia/shop/CommonUtils.groovy
@@ -4,7 +4,7 @@
 package com.nghia.shop
 
 import java.util.List;
-
+import org.springframework.web.context.request.RequestContextHolder as RCH
 import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap;
 
 /**
@@ -154,4 +154,106 @@ class CommonUtils {
 			 .replace("%", "\\%")
 		 } else return null
 	 }
+	 
+	 static validateParamsForPaging(GrailsParameterMap params, Object gotoPage, long totalRecordCount, long recordNumberPerPage = 10, long maxRecordNumberPerPage = 100, List<String> listSortBy){
+		 
+				 def rs = params
+				 def max = rs.max // input by user
+				 def offset = rs.offset // input by user
+				 def gotoPg
+				 def sortBy = rs.sort // input by user
+				 def orderBy = rs.order // input by user
+		 
+				 // validate for rs.max
+				 if (max == null) {
+					 rs.max = recordNumberPerPage
+		 
+				 } else {
+					 try {
+		 
+						 rs.max = max.toString().toInteger()
+						 if (rs.max <= 0) {
+							 rs.max = recordNumberPerPage // set default record number per page
+						 } else {
+							 rs.max = Math.min(rs.max, maxRecordNumberPerPage)
+						 }
+		 
+					 } catch (NumberFormatException e) {
+						 rs.max = recordNumberPerPage // set default record number per page
+					 }
+				 }
+		 
+				 // validate for rs.offset
+				 if (offset == null) {
+					 rs.offset = 0
+				 } else {
+					 try {
+		 
+						 rs.offset = offset.toString().toInteger()
+		 
+						 if (rs.offset < 0){
+							 rs.offset = 0
+						 }
+		 
+						 // in case input offset greater maximum offset
+						 else if (rs.offset >= totalRecordCount) {
+							 rs.offset = Math.floor((totalRecordCount-1) / rs.max) * rs.max
+						 }
+						 else if (rs.offset % rs.max != 0) {
+							 rs.offset = Math.floor(rs.offset / rs.max) * rs.max
+						 }
+		 
+					 } catch (NumberFormatException e) {
+						 rs.offset = 0
+					 }
+				 }
+		 
+				 // validate for gotoPage and then return rs.offset (with rs.max gets value default by recordNumberPerPage)
+				 if (gotoPage != null){
+					 try {
+		 
+						 gotoPg = gotoPage.toString().toLong() // input by user
+		 
+						 def maxPage
+		 
+						 if (totalRecordCount % rs.max > 0) {
+							 maxPage = Math.floor(totalRecordCount / rs.max) + 1
+						 } else {
+							 maxPage = totalRecordCount / rs.max
+						 }
+		 
+						 if (gotoPg <= 0 ) {
+							 rs.offset = 0 // go to page 1
+		 
+						 } else if(gotoPg >= 1 && gotoPg <= maxPage) {
+							 rs.offset = (gotoPg - 1) * rs.max
+						 } else {
+							 rs.offset = Math.floor((totalRecordCount-1) / rs.max) * rs.max
+						 }
+		 
+					 } catch (NumberFormatException e) {
+						 rs.offset = 0 // go to page 1
+					 }
+				 }
+		 
+				 // validate for sort and order
+				 if (listSortBy != null) {
+					 if (sortBy != null){
+		 
+						 if (orderBy != null && !"asc".equals(orderBy.toString().toLowerCase()) && !"desc".equals(orderBy.toString().toLowerCase())) {
+							 rs.sort = null
+							 rs.order = null
+		 
+						 } else {
+							 if (!listSortBy.contains(sortBy)){
+								 rs.sort = null
+								 rs.order = null
+							 }
+						 }
+					 }
+				 }
+		 
+				 params = rs
+				 return params
+			 }
 }
