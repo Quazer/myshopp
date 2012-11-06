169438cc5256e98e8c9f8dce668197e6a66e30f3
tv98.net <tv98.net@gmail.com>
2012-11-06 11:25:08 +0000

    add value for some properties

--- shop/grails-app/conf/BootStrap.groovy
+++ shop/grails-app/conf/BootStrap.groovy
@@ -12,24 +12,41 @@ class BootStrap {
 		def admin = new Member(username: "nghia", password: "asdasd").save(flush:true)
 		def supplier1 = new Member(username: "supplier1", password: "asdasd").save(flush:true)
 		def supplier2 = new Member(username: "supplier2", password: "asdasd").save(flush:true)
-		def prod
+		def cat1 = new Category(name : "May vi tinh").save(flush:true)
+		cat1.save(flush:true)
+		
 		for (int i = 0; i<= 20; i++) {
-			prod = new Product(name : "product " + i, overview: "overview of " + i,
-				specification : "specification of " + i)
+			def prod = new Product()
+			prod.name = "product " + i
+			prod.overview = "overview of " + i
+			prod.specification = "specification of " + i
+			
+			prod.purchargePrice = 100
+			prod.importPrice = 76
+			prod.discount = 0
+			prod.discount3Item = 0
+			prod.discount5Item = 0
+			prod.discount10Item = 0
+			
+			prod.SKU = 1000 + i
+			
+			prod.category = cat1
+//			prod = new Product(name : "product " + i, overview: "overview of " + i,
+//				specification : "specification of " + i)
 //			prod.addToSupplier(supplier1)
 //			prod.addToCategory(cat1)
 			prod.save(flush:true)
 			print "prod " + i + " :" + prod.id
 		}
 		
-		def cat1 = new Category(name : "May vi tinh").save(flush:true)
-		cat1.addToProducts(prod)
-		cat1.save(flush:true)
+//		def cat1 = new Category(name : "May vi tinh").save(flush:true)
+//		cat1.addToProducts(prod)
+//		cat1.save(flush:true)
 		print "cat1 :" + cat1.id
-		def cat2 = new Category(name : "Iphone case").save(flush:true)
-		cat2.addToProducts(prod)
-		cat2.save(flush:true)
-		print "cat2 :" + cat2.id
+//		def cat2 = new Category(name : "Iphone case").save(flush:true)
+//		cat2.addToProducts(prod)
+//		cat2.save(flush:true)
+		//print "cat2 :" + cat2.id
 	}
 	
 	
