4508d1be14ade87f5ab3546991acf0950f306723
tv98.net@gmail.com <tv98.net@gmail.com@c073b083-006d-3139-c555-1cae7268bc85>
2012-11-19 10:54:52 +0000

    add/edit some prop

--- trunk/shop/grails-app/domain/com/nghia/shop/ShoppingCart.groovy
+++ trunk/shop/grails-app/domain/com/nghia/shop/ShoppingCart.groovy
@@ -3,16 +3,30 @@ package com.nghia.shop
 class ShoppingCart {
 	Product product
 	Member member
-	
+	long orderNumber
 	int quantity = 1
 	int giftQuantity = 0
 	int shippedQuantity = 0
 	ShippingMethod shippingMethod
+	float shippingMethodPrice
+	
+	String messageToShop = ""
 	
 	Date dateAdded = new Date()
+	
+	boolean isConfirmed
+	boolean isCheckout
+	float discount = 0
 	
 	//TODO: need to modify
     static constraints = {
 		shippingMethod nullable:true
+		messageToShop blank:true
+		
+//		year(matches:/\d{4}/)
+//		month(matches:/\d{2}/)
+//		day(matches:/\d{2}/)
+		orderNumber unique:true, nullable:true
     }
+
 }
