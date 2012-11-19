d77a867ab1517155d0735e8f49d6edb22e704ba5
tv98.net@gmail.com <tv98.net@gmail.com@c073b083-006d-3139-c555-1cae7268bc85>
2012-11-19 10:55:23 +0000

    add note

--- trunk/shop/grails-app/services/com/nghia/shop/ProductService.groovy
+++ trunk/shop/grails-app/services/com/nghia/shop/ProductService.groovy
@@ -3,6 +3,8 @@ package com.nghia.shop
 class ProductService {
 
 	static transactional = true
+	
+	//TODO: need to remove this method????
     def autoCreate(supplier1, cat1) {
 		for (int i = 0; i<= 20; i++) {
 			def prod = new Product(name : "product " + i, overview: "overview of " + i,
