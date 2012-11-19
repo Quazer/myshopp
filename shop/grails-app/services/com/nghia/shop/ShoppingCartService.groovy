9bfd65053eb71b7ffcf5f71dacafec8d90685c2c
tv98.net <tv98.net@gmail.com>
2012-11-19 10:55:36 +0000

    create

---
+++ shop/grails-app/services/com/nghia/shop/ShoppingCartService.groovy
@@ -0,0 +1,54 @@
+package com.nghia.shop
+
+class ShoppingCartService {
+	static transactional = true
+	
+	def checkout(productIds, currentuserLogin, params) {
+		def error
+		def tmp
+		def shoppingCartList = new ArrayList()
+		for (def productId : productIds) {
+			if (productId.matches("\\d{1,12}")) {
+				productId = Long.parseLong(productId)
+				if (productId > 0) {
+					tmp = "messageToShop-" + productId
+					if (params."${tmp}"?.trim()) {
+						def productInstance = Product.get(productId)
+						if (productInstance) {
+							ShoppingCart shoppingCart = ShoppingCart.findByProductAndMember(productInstance, currentuserLogin)
+							if (shoppingCart && !error &&shoppingCart.isCheckout == false) {
+								shoppingCart.messageToShop = params."${tmp}".trim()
+								shoppingCart.isConfirmed = true
+								shoppingCart.orderNumber = CommonUtils.generateOrderNumber(shoppingCart)
+								if (shoppingCart.hasErrors()) {
+									error = true
+									break
+								}
+								else {
+									shoppingCartList.add(shoppingCart)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		if (!error && shoppingCartList.size > 0) {
+			def orderStatusTracking = new OrderStatusTracking()
+			orderStatusTracking.status = 3
+			
+			for (def shoppingItem : shoppingCartList) {
+				orderStatusTracking.addToShoppingCart(shoppingItem)
+			}
+			
+			orderStatusTracking.save(flush:true)
+			print orderStatusTracking.date.time
+			
+			return orderStatusTracking
+		}
+		else {
+			return null
+		}
+	}
+}
